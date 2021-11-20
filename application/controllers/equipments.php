7ab3a4cadc91208734c05ebda7e48805df2b7da3
Manish <sadhumanishkumar@gmail.com>
2021-11-20 22:12:24 +0530

    Feat : access ctrls for edit form

--- application/controllers/equipments.php
+++ application/controllers/equipments.php
@@ -24,33 +24,77 @@ class Equipments extends CI_Controller {
 
     function add(){
 		if($this->session->userdata('logged_in')){
-			$this->load->helper('form');
-			$this->load->library('form_validation');
-			$this->data['title']='Add Equipment';
-			$this->load->view('templates/header' , $this->data);
-			$this->data['equipment_type'] = $this->master_model->get_data('equipment_type');
-			$this->data['equipment_category'] = $this->master_model->get_data('equipment_category');
-			$this->data['location'] = $this->master_model->get_data('location');
-			$this->data['party'] = $this->master_model->get_data('party');
-			$this->data['equipment_procurement_type'] = $this->master_model->get_data('equipment_procurement_type');
-			$this->data['equipment_procurement_status'] = $this->master_model->get_data('equipment_procurement_status');
-			$this->data['equipment_functional_status'] = $this->master_model->get_data('equipment_functional_status');
-			$this->data['journal_type'] = $this->master_model->get_data('journal_type');
-			$this->form_validation->set_rules('equipment_name','equipment_name','required');
-			if ($this->form_validation->run() === FALSE) {
-				$this->load->view('add_equipment',$this->data);
-			} else {
-				if($this->master_model->add_equipment()){
-					$this->data['msg']="Equipment added successfully";
+			$add_equipment_access=0; 
+			foreach($this->data['functions'] as $f){
+				if($f->user_function=="equipment"){ 
+					if($f->add)
+						$add_equipment_access=1;  	
+				}	
+			}
+			if($add_equipment_access){
+				$this->load->helper('form');
+				$this->load->library('form_validation');
+				$this->data['title']='Add Equipment';
+				$this->load->view('templates/header' , $this->data);
+				$this->data['equipment_type'] = $this->master_model->get_data('equipment_type');
+				$this->data['equipment_category'] = $this->master_model->get_data('equipment_category');
+				$this->data['location'] = $this->master_model->get_data('location');
+				$this->data['party'] = $this->master_model->get_data('party');
+				$this->data['equipment_procurement_type'] = $this->master_model->get_data('equipment_procurement_type');
+				$this->data['equipment_procurement_status'] = $this->master_model->get_data('equipment_procurement_status');
+				$this->data['equipment_functional_status'] = $this->master_model->get_data('equipment_functional_status');
+				$this->data['journal_type'] = $this->master_model->get_data('journal_type');
+				$this->form_validation->set_rules('equipment_name','equipment_name','required');
+				if ($this->form_validation->run() === FALSE) {
 					$this->load->view('add_equipment',$this->data);
 				} else {
-					$this->data['msg']="Error adding equipment. Please retry.";
-					$this->load->view('add_equipment',$this->data);
+					if($this->master_model->add_equipment()){
+						$this->data['msg']="Equipment added successfully";
+						$this->load->view('add_equipment',$this->data);
+					} else {
+						$this->data['msg']="Error adding equipment. Please retry.";
+						$this->load->view('add_equipment',$this->data);
+					}
 				}
+				$this->load->view('templates/footer' ,$this->data);
+			} else {
+				show_404();	
 			}
-			$this->load->view('templates/footer' ,$this->data);
 		} else{
 			show_404();
 		}
     }
+
+	function edit($equipment_id){
+		if($this->session->userdata('logged_in')){
+			$edit_equipment_access=0; 
+			foreach($this->data['functions'] as $f){
+				if($f->user_function=="equipment"){ 
+					if($f->edit)
+						$edit_equipment_access=1;  	
+				}	
+			}
+			if($edit_equipment_access){
+				$this->data['title']='Equipment';
+				$this->load->view('templates/header' , $this->data);
+				$this->data['equipment_id'] = $equipment_id;
+				$this->data['equipment_type'] = $this->master_model->get_data('equipment_type');
+				$this->data['equipment_category'] = $this->master_model->get_data('equipment_category');
+				$this->data['location'] = $this->master_model->get_data('location');
+				$this->data['party'] = $this->master_model->get_data('party');
+				$this->data['equipment_procurement_type'] = $this->master_model->get_data('equipment_procurement_type');
+				$this->data['equipment_procurement_status'] = $this->master_model->get_data('equipment_procurement_status');
+				$this->data['equipment_functional_status'] = $this->master_model->get_data('equipment_functional_status');
+				$this->data['journal_type'] = $this->master_model->get_data('journal_type');
+				$this->data['party'] = $this->master_model->get_data('party');
+				$this->data['equipment'] = $this->master_model->get_equipment_by_id($equipment_id);
+				$this->load->view('edit_equipment', $this->data);
+				$this->load->view('templates/footer' ,$this->data);
+			} else {
+				show_404();	
+			}
+		}  else{
+			show_404();
+		}
+	}
 }
