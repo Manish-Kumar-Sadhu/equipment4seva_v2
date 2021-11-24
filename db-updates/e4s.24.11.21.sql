e4afe45a3a0697d56e6876414ab5a41f90609651
Manish <sadhumanishkumar@gmail.com>
2021-11-24 11:15:21 +0530

    Feat : party table alterations

--- db-updates/e4s.24.11.21.sql
+++ db-updates/e4s.24.11.21.sql
@@ -10,3 +10,16 @@ ALTER TABLE `party` CHANGE `party_address` `party_address` VARCHAR(100) CHARACTE
 
 ALTER TABLE `party` CHANGE `party_name` `party_name` VARCHAR(70) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL;
 
+--
+-- Added party_bank_ifsc column to party table
+--
+ALTER TABLE `party` ADD `bank_branch_ifsc` VARCHAR(50) NULL DEFAULT NULL AFTER `bank_branch`;
+
+--
+-- Added created_by, created_datetime, updated_by and updated_datetime to party table
+--
+ALTER TABLE `party` 
+ADD `created_by` INT(11) NULL DEFAULT NULL AFTER `party_pan`, 
+ADD `created_datetime` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `created_by`, 
+ADD `updated_by` INT(11) NULL DEFAULT NULL AFTER `created_datetime`,
+ADD `updated_datetime` DATETIME NULL DEFAULT NULL AFTER `updated_by`;
