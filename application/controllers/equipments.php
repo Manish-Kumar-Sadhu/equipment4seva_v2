648e886fdb483abed2b8701f193000e232ac019e
Manish <sadhumanishkumar@gmail.com>
2021-11-27 12:24:14 +0530

    Feat : location history in view equip detaisls

--- application/controllers/equipments.php
+++ application/controllers/equipments.php
@@ -29,6 +29,7 @@ class Equipments extends CI_Controller {
 		$this->data['equipment_type'] = $this->master_model->get_data('equipment_type');
 		$this->data['equipment_category'] = $this->master_model->get_data('equipment_category');
 		$this->data['location'] = $this->master_model->get_data('location');
+		$this->data['equipment_location_history'] = $this->master_model->get_equipment_location_history($equipment_id);
 		$this->data['district'] = $this->master_model->get_data('district');
 		$this->data['journal_type'] = $this->master_model->get_data('journal_type');
 		$this->data['party'] = $this->master_model->get_data('party');
