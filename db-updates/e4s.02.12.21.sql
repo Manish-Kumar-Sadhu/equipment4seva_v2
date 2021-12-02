f714229522873c28db3e0ca80d0780e7f6f426b9
Manish <sadhumanishkumar@gmail.com>
2021-12-02 19:39:01 +0530

    Feat: new user function for party

---
+++ db-updates/e4s.02.12.21.sql
@@ -0,0 +1,5 @@
+--
+-- new user function for party
+--
+INSERT INTO `user_function` (`user_function_id`, `user_function`, `user_function_display`, `description`) 
+VALUES (NULL, 'party', 'Party', '');
