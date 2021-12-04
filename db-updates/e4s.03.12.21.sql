059aa7d7b3c248b52da8ce6b4eaf8e2aced94831
Manish <sadhumanishkumar@gmail.com>
2021-12-04 19:55:04 +0530

    chore : note column in party table

--- db-updates/e4s.03.12.21.sql
+++ db-updates/e4s.03.12.21.sql
@@ -7,3 +7,8 @@ ALTER TABLE `user_party_link` DROP `is_default_party`;
 --- Added default_party_id column to user table
 ---
 ALTER TABLE `user` ADD `default_party_id` INT(11) NULL DEFAULT NULL AFTER `password`;
+
+---
+--- Added note column to party table
+---
+ALTER TABLE `party` ADD `note` TEXT NULL DEFAULT NULL AFTER `party_pan`;
