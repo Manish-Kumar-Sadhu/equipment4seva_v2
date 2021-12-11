8531aa37f4258775d84c5f1503fd82beb9207830
Manish <sadhumanishkumar@gmail.com>
2021-12-11 14:41:10 +0530

    Feat : query to fetch documents by equip id

--- application/models/documentation_model.php
+++ application/models/documentation_model.php
@@ -19,5 +19,22 @@ class documentation_model extends CI_Model{
         $this->db->trans_complete(); //Transaction Ends
 		if($this->db->trans_status()===TRUE) return true; else return false; //if transaction completed successfully return true, else false.
 	}
+
+	function get_documents_by_equipment_id($equipment_id){
+		$this->db->select('document_type, document_date, created_user.first_name as created_user_first_name, created_user.last_name  as created_user_last_name,
+		document_link, equipment_documents.note as equipment_document_note')
+		->from('equipment_documents')
+		->join('equipment_document_type','equipment_document_type.document_type_id = equipment_documents.document_type_id','left')
+		->join('user as created_user','created_user.user_id=equipment_documents.created_by','left')
+		->where("equipment_id",$equipment_id)
+		->order_by('equipment_documents.create_datetime');
+		$query = $this->db->get();
+        $result =  $query->result();
+        if($result){
+            return $result;       
+        }else{
+            return false;
+        }
+	}
     
 }
