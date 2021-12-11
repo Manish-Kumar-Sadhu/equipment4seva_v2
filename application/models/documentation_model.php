0e464486525eeb34412519c92242d3ce59d30c45
Manish <sadhumanishkumar@gmail.com>
2021-12-11 13:50:27 +0530

    Feat : add_document query

--- application/models/documentation_model.php
+++ application/models/documentation_model.php
@@ -4,5 +4,20 @@ class documentation_model extends CI_Model{
 		parent::__construct();
 	}
 
+	function add_document($filename, $equipment_id){
+		$data = array(
+			'equipment_id'=>$equipment_id,
+			'document_date'=>$this->input->post('document_date'),
+			'document_type_id'=>$this->input->post('document_type'),
+			'note'=>$this->input->post('note'),
+			'document_link' => $filename,
+			'created_by'=>$this->session->userdata('logged_in')['user_id']
+		);
+
+		$this->db->trans_start(); //Transaction begins
+        $this->db->insert('equipment_documents',$data); //Insert 
+        $this->db->trans_complete(); //Transaction Ends
+		if($this->db->trans_status()===TRUE) return true; else return false; //if transaction completed successfully return true, else false.
+	}
     
 }
